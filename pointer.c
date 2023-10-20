5022297ff73341058a184735f04cc00617a39955
oladeinde maryam <mariamoladeinde19@gmail.com>
2023-10-20 10:10:23 +0100

    incrementing pointer array

---
+++ pointer.c
@@ -0,0 +1,33 @@
+# include<stdio.h>
+#include<stdint.h>
+
+int increment(uint16_t *arr){
+for(int i =0; i< 5; i++){
+     arr[i]++;
+    
+}
+
+}
+
+int main(){
+    uint16_t a[]={1,2,3,4,5};
+    increment(a);
+    for (int i = 0; i < 5; i++)
+    {
+    printf("the value is %d\n",a[i]);
+    printf("the address is %d\n",&a[i]);
+    }
+    
+}
+
+
+
+
+
+   
+
+
+
+   
+
+ 
